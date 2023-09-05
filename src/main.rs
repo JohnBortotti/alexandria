dbf899c3740ec34d7da1418db5edd680f3bd0928
JohnBortotti <joao_bortotti@hotmail.com>
2023-09-05 20:29:17 -0300

    removing unused comments (projects notes and ideas)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-// Ideas
-// 
-// description: create a simple relational database, storing data (normal tables) with some kind of Btree
-// and Raft as consensus algorithm. Not inteended to be fully deployable, just simulate some
-// operations, consensus, memory management, async and distributed computation.
-//
-// planning: -> API (cli, http, async server, dont know yet) 
-//           -> SQL front-end (lex, parse, planner, etc) 
-//           -> Raft backend (leader, replication, commit and log) 
-//           -> Storage backend (SQL State machine) 
-//
-// Raft: -> base algorithm 
-//       -> log database (append-file)
-//
-// DB engine: -> simple B-tree variation (B+tree, etc)
-//            -> implement Store operations (set, get, delete, flush)
-//
-// SQL: -> data types (bool, int, float, string)
-//      -> single x multiple databases??
-//      -> schema operations (create, delete, read, update??)
-//      -> implement transactions for row operations (ACID)
-
-// ticks
-// set tick from miliseconds
-// set tick intervals (steps and elections)
-// create tokio timer (on node eventLoop)
-// implement each role tick
-
 #[allow(dead_code)]
 mod raft;
 
