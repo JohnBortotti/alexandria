d210b9473827d24888a210428f379d6c353d2a78
JohnBortotti <joao_bortotti@hotmail.com>
2023-09-05 11:40:53 -0300

    expose tcp linstener on main.rs (only testing)

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,15 @@
 mod raft;
 
 use std::collections::HashMap;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let server = raft::server::Server::new("a", HashMap::new(), raft::node::Log::new()).await;
-    server.serve().await;
+    let server = raft::server::Server::new("127.0.0.1:8080", HashMap::new(), raft::node::Log::new()).await;
+    let tcp_listener = match TcpListener::bind("127.0.0.1:8080").await {
+        Ok(listener) => listener,
+        _ => panic!("TCPListener bind error")
+    };
+
+    let _ = server.serve(tcp_listener).await;
 }
