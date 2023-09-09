a41fec46261425cc416236dcdbdb76b1ad3c32ef
JohnBortotti <joao_bortotti@hotmail.com>
2023-09-09 18:59:31 -0300

    update raft/server to send tcp messages to other peers

--- src/raft/server.rs
+++ src/raft/server.rs
@@ -1,28 +1,29 @@
-use super::{node, message};
-use std::collections::HashMap;
+use super::{node, message, message::Address::{Broadcast, Peer}};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel};
 use tokio::net::TcpListener;
 use tokio_stream::wrappers::{UnboundedReceiverStream, TcpListenerStream};
 use tokio_stream::StreamExt as _;
 use std::time::Duration;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 const TICK: Duration = Duration::from_millis(100);
 
 pub struct Server {
     node: node::Node,
-    peers: HashMap<String, String>,
+    peers: Vec<String>,
     node_rx: UnboundedReceiver<message::Message>,
 }
 
 impl Server {
     pub async fn new(
         id: &str,
-        peers: HashMap<String, String>,
+        peers: Vec<String>,
         log: node::Log,
         ) -> Self {
         let (node_tx, node_rx) = unbounded_channel();
         Self {
-            node: node::Node::new(id, peers.keys().map(|k| k.to_string()).collect(), log, node_tx).await,
+            node: node::Node::new(id, peers.clone(), log, node_tx).await,
             peers,
             node_rx,
         }
@@ -31,7 +32,8 @@ impl Server {
     pub async fn serve(self, tcp_listener: TcpListener) -> Result<(), &'static str> {
         // TODO: test single thread vs multithreading (spawn or forwarding messages on single runtime)
         let (tcp_inbound_tx, tcp_inbound_rx) = unbounded_channel::<message::Message>();
-        tokio::spawn(Self::handle_inbound_tcp(tcp_listener, tcp_inbound_tx));
+        tokio::spawn(Self::inbound_receiving_tcp(tcp_listener, tcp_inbound_tx));
+        tokio::spawn(Self::inbound_sending_tcp(self.node_rx, self.peers));
         Self::event_loop(self.node, tcp_inbound_rx).await
     }
 
@@ -48,11 +50,11 @@ impl Server {
                 Some(msg) = tcp_rx.next() => node = node.step(msg)?
             }
 
-            // TODO: match node sending message (node_rx -> another peer)
+            // TODO: code for peers sending messages (handle message routing)
         }
     }
 
-    async fn handle_inbound_tcp(
+    async fn inbound_receiving_tcp(
         listener: TcpListener, 
         tcp_inbound_tr: UnboundedSender<message::Message>
         ) -> Result<(), std::io::Error> {
@@ -72,17 +74,48 @@ impl Server {
                         .skip_while(|x| !x.is_empty())
                         .collect();
 
+                    if let None = &req_body.get(1) {
+                        panic!("message incorrect");
+                    };
+
                     // example payload: "(term:1,from:Broadcast,to:Broadcast,event:AppendEntries(index:1,term:1))"
                     let parsed_msg: message::Message = ron::from_str(&req_body[1]).unwrap();
                     let _ = tcp_tr.send(parsed_msg).unwrap();
 
                     let _ = &socket.writable();
-                    let res = "HTTP/1.1 201 OK\r\n";
+                    let res = "HTTP/1.1 200 OK\r\n";
                     let _ = socket.try_write(res.as_bytes());
                 }
                 Err(e) => {
                     eprintln!("Erro ao ler os dados: {}", e);
                 }
+            }
+        };
+
+        Ok(())
+    }
+
+    async fn inbound_sending_tcp(
+        node_rx: UnboundedReceiver::<message::Message>,
+        peers: Vec<String>
+        ) -> Result<(), std::io::Error> {
+        let mut listener = UnboundedReceiverStream::new(node_rx);
+
+        while let Some(msg) = listener.next().await {
+            let serialized_msg = ron::to_string(&msg).unwrap();
+            let http_packet = format!("HTTP/1.1 200 OK \n\n{}", serialized_msg);
+
+            match msg.to {
+                Broadcast => {
+                    peers.iter().for_each(|peer| {
+                        let mut stream = TcpStream::connect(peer).unwrap();
+                        stream.write(http_packet.as_bytes()).unwrap();
+                    });
+                },
+                Peer(addr) => {
+                    let mut stream = TcpStream::connect(addr).unwrap();
+                    stream.write(http_packet.as_bytes()).unwrap();
+                },
             }
         };
 
