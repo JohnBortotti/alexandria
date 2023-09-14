e640d22c6a220d57cda3a290e8339452e07cf542
JohnBortotti <joao_bortotti@hotmail.com>
2023-09-14 13:15:05 -0300

    Fix docker-compose network to connnect the peers
    Fix docker compose networking
    Fix raft tcp bind address (from 127.0.0.1 to 0.0.0.0)

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,11 @@ async fn main() {
         peers,
         raft::node::Log::new(),
         ).await;
-    let tcp_listener = match TcpListener::bind("127.0.0.1:8080").await {
+
+    let tcp_listener = match TcpListener::bind("0.0.0.0:8080").await {
         Ok(listener) => listener,
         _ => panic!("TCPListener bind error")
     };
+
     let _ = server.serve(tcp_listener).await;
 }
